1d019748ed06a7d3c589b57090e0c381c425f5ae
Paul Smedley <paul@smedley.id.au>
2023-03-08 06:59:06 +1030

    Update source to 6.4.2
    Signed-off-by: Paul Smedley <paul@smedley.id.au>

--- tests/auto/webchannel/tst_webchannel.cpp
+++ tests/auto/webchannel/tst_webchannel.cpp
@@ -1,31 +1,6 @@
-/****************************************************************************
-**
-** Copyright (C) 2016 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com, author Milian Wolff <milian.wolff@kdab.com>
-** Copyright (C) 2019 Menlo Systems GmbH, author Arno Rehn <a.rehn@menlosystems.com>
-** Contact: https://www.qt.io/licensing/
-**
-** This file is part of the QtWebChannel module of the Qt Toolkit.
-**
-** $QT_BEGIN_LICENSE:GPL-EXCEPT$
-** Commercial License Usage
-** Licensees holding valid commercial Qt licenses may use this file in
-** accordance with the commercial license agreement provided with the
-** Software or, alternatively, in accordance with the terms contained in
-** a written agreement between you and The Qt Company. For licensing terms
-** and conditions see https://www.qt.io/terms-conditions. For further
-** information use the contact form at https://www.qt.io/contact-us.
-**
-** GNU General Public License Usage
-** Alternatively, this file may be used under the terms of the GNU
-** General Public License version 3 as published by the Free Software
-** Foundation with exceptions as appearing in the file LICENSE.GPL3-EXCEPT
-** included in the packaging of this file. Please review the following
-** information to ensure the GNU General Public License requirements will
-** be met: https://www.gnu.org/licenses/gpl-3.0.html.
-**
-** $QT_END_LICENSE$
-**
-****************************************************************************/
+// Copyright (C) 2016 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com, author Milian Wolff <milian.wolff@kdab.com>
+// Copyright (C) 2019 Menlo Systems GmbH, author Arno Rehn <a.rehn@menlosystems.com>
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
 
 #include "tst_webchannel.h"
 
@@ -1022,7 +997,7 @@ void TestWebChannel::testWrapObjectWithMultipleTransports()
     pub->wrapResult(QVariant::fromValue(&obj), dummyTransport);
     pub->wrapResult(QVariant::fromValue(&obj), dummyTransport2);
 
-    QCOMPARE(pub->transportedWrappedObjects.count(), 2);
+    QCOMPARE(pub->transportedWrappedObjects.size(), 2);
 }
 
 void TestWebChannel::testJsonToVariant()
@@ -1584,9 +1559,9 @@ void TestWebChannel::qtbug46548_overriddenProperties()
     QSignalSpy spy(&engine, &TestJSEngine::channelSetupReady);
     connect(&engine, &TestJSEngine::channelSetupReady, TestSubclassedFunctor(&engine));
     engine.initWebChannelJS();
-    if (!spy.count())
+    if (!spy.size())
         spy.wait();
-    QCOMPARE(spy.count(), 1);
+    QCOMPARE(spy.size(), 1);
     QJSValue subclassedTestObject = engine.evaluate("channel.objects[\"subclassedTestObject\"]");
     QVERIFY(subclassedTestObject.isObject());
 
